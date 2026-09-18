b9a43e6797a8954146058ccc35b38cff98547b8c
UserTar <daneildam7677@gmail.com>
2026-09-18 16:32:56 +0200

    Clase

--- app/src/main/java/com/daniel/homestock/AddObjectScreen.kt
+++ app/src/main/java/com/daniel/homestock/AddObjectScreen.kt
@@ -6,17 +6,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.material3.TextField
+
 @Composable
 fun AddObjectScreen( onBackClick: () -> Unit) {
+
+    var nombre by remember {mutableStateOf(" ")}
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Añadir objeto")
+
+        TextField(
+            value = nombre,
+            onValueChange = {
+                nombre = it //Es una función lambda guardamos el valor que acabamos de introducir "it"
+            },
+            label = {
+                Text("Nombre del objeto")
+            }
+        )
+
+        Text(
+            text = "Objeto: $nombre"
+        )
 
         Button(
             onClick = {
